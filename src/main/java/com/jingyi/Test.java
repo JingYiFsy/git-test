799903a0ada03a64fa197eaec996a1f4583dc7d5
jingyi <83011160+JingYiFsy@users.noreply.github.com>
2022-10-20 09:35:19 +0800

    pull commit

--- src/main/java/com/jingyi/Test.java
+++ src/main/java/com/jingyi/Test.java
@@ -10,5 +10,8 @@ public class Test {
         System.out.println("hello test");
         System.out.println("push test");
         System.out.println("push test");
+        System.out.println("pull test");
+        System.out.println("pull test");
+        
     }
 }
