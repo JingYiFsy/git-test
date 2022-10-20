8fad7243fdc1ae946f76f17433b57639eb8fc3b5
jingyi <935160593@qq.com>
2022-10-20 09:11:33 +0800

    master test

--- src/main/java/com/jingyi/Test.java
+++ src/main/java/com/jingyi/Test.java
@@ -5,5 +5,6 @@ public class Test {
         System.out.println("hello git!");
         System.out.println("你好");
         System.out.println("你好");
+        System.out.println("master test");
     }
 }
